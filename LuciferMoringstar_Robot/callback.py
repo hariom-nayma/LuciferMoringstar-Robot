1c2247cbcdafc864cc8e598ea4f1fce19eb41c84
Hariom Nayma <74749873+hariom-nayma@users.noreply.github.com>
2022-03-18 14:02:39 +0800

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -250,7 +250,7 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                 if f_caption is None:
                     f_caption = LuciferMoringstar.FILE_CAPTIONS
                 buttons = [[
-                  InlineKeyboardButton('🧑‍💻 How To Own 🧑‍💻', url='https://t.me/howtodoyt')
+                  InlineKeyboardButton('🧑‍💻 How To Own 🧑‍💻', url='https://t.me/sadsoul_bot')
                   ]]                 
                 
                 await query.answer()
@@ -273,8 +273,8 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                  InlineKeyboardButton("ℹ️ Help", callback_data="help"),
                  InlineKeyboardButton("😎 About", callback_data="about") 
                  ],[
-                 InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/LkY2feKT2k0"),
-                 InlineKeyboardButton("🤖 Update", url="https://t.me/HTDGROUPFORMOVIES")
+                 InlineKeyboardButton("🗳 Instagram", url="https://instagram.com/hariom.0_0.nayma"),
+                 InlineKeyboardButton("🤖 Update", url="https://t.me/+y0AKZbkOoZdmNzVl")
                  ]]
             else:
                 buttons = [[
@@ -283,8 +283,8 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                  InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
                  InlineKeyboardButton("😎 About", callback_data="about") 
                  ],[
-                 InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/LkY2feKT2k0"),
-                 InlineKeyboardButton("🤖 Update", url="https://t.me/HTDGROUPFORMOVIES")
+                 InlineKeyboardButton("🗳 Instagram", url="https://instagram.com/hariom.0_0.nayma"),
+                 InlineKeyboardButton("🤖 Update", url="https://t.me/+y0AKZbkOoZdmNzVl")
                  ]]               
             await query.message.edit(text=START_MSG.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
